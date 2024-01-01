8c5526abd5fd65b7c3b2ef99da845713ecb2b94e
Maxime Bouyssou <maxime.bouyssou@ergoss.fr>
2024-01-01 18:37:57 +0100

    FIX(auth) : check if user stil exist in dB, if not token is invalid

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
+const dbUser = require('../DB/DataAccess').User;
 require('dotenv').config();
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     try {
         let token = req.headers.authorization.split(' ')[1];
         jwt.verify(token, process.env.TOKEN_KEY);
+        const user = jwt.decode(token, process.env.TOKEN_KEY);
+        const isUserExist = (await dbUser.findOne({ where: { id: user.id } })) != null;
+        if (!isUserExist)
+            throw new Error('Invalid token');
+        
         next();
     } catch (error) {
         res.status(401).json({ message: "Authenticated route, identify yourself with a valid JWT." });
