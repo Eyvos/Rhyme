916ddfed2c6c277a4eb96ce1d0f8df97e4a87281
buysu-m <137394027+buysu-m@users.noreply.github.com>
2024-01-01 13:33:54 +0100

    Revert "Revert "Ressource user""

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,10 @@ const userController = require('../controllers/user');
 const auth = require('../middlewares/authMiddleware');
 
 router.get('/', auth, userController.getAll);
-router.get('/:id', auth, userController.getOne);
+router.get('/:id', userController.getOne);
+router.put('/', auth, userController.changeUsername);
+router.delete('/:id', auth, userController.deleteUserAdmin);
+router.put('/password', auth, userController.changePassword)
+router.delete('/', auth, userController.deleteUser);
 
 module.exports = router;
