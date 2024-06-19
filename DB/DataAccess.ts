457ad80d2df19a7f1f6ec5ef976704e9af63c3b3
Eyvos <elvis.pichou@gmail.com>
2024-06-19 11:02:20 +0200

    :bug: FIX(DataAccess): Better syncing

--- DB/DataAccess.ts
+++ DB/DataAccess.ts
@@ -1,93 +1,95 @@
 import { DataTypes, Model, Sequelize } from 'sequelize';
 import { IRhyme } from '../models/rhyme';
 import { IUser } from '../models/user';
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
+
 dotenv.config();
 
-interface UserModel extends IUser, Model<IUser> {}
-interface RhymeModel extends Model<IRhyme>, IRhyme {}
-
+interface UserModel extends IUser, Model<IUser> { }
+interface RhymeModel extends Model<IRhyme>, IRhyme { }
 
 let sequelize: Sequelize;
-if (process.env.DB_NAME && process.env.DB_USER && process.env.DB_PASSWORD && process.env.DB_HOST) 
-{
+
+if (process.env.DB_NAME && process.env.DB_USER && process.env.DB_PASSWORD && process.env.DB_HOST) {
     sequelize = new Sequelize({
         database: process.env.DB_NAME,
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
         dialect: 'mysql',
-        port: 3306
+        port: 3306,
+        logging: false, // Désactivez le logging pour éviter les informations inutiles en production
     });
-}
-else{
+} else {
     throw new Error('Database configuration not found');
 }
 
-// interface RhymeInstance extends Sequelize.Instance<IRhyme>, IRhyme {}
-
-//#region define post table
+// Définir le modèle Rhyme
 const Rhyme = sequelize.define<RhymeModel>('Rhyme', {
     title: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
     },
     content: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
     },
     userId: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
     },
     parentId: {
         type: DataTypes.INTEGER,
-        allowNull: true
+        allowNull: true,
     },
     isGenerated: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
-        defaultValue: false
-    
-    }
+        defaultValue: false,
+    },
 });
-Rhyme.hasMany(Rhyme, { foreignKey: 'parentId', sourceKey: 'id' });
-//#endregion
 
-//#region define user table
-const User = sequelize.define<UserModel>('user', {
+// Définir la relation Rhyme
+Rhyme.hasMany(Rhyme, { foreignKey: 'parentId', sourceKey: 'id' });
+
+// Définir le modèle User
+const User = sequelize.define<UserModel>('User', {
     email: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: true, // Ajout d'une contrainte d'unicité pour l'email
     },
     username: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: true, // Ajout d'une contrainte d'unicité pour le nom d'utilisateur
     },
     password: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
     },
-    image : {
+    image: {
         type: DataTypes.TEXT,
-        allowNull: true
+        allowNull: true,
     },
     isAdmin: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
-        defaultValue: false
-    }
+        defaultValue: false,
+    },
 }, {
     defaultScope: {
         attributes: { exclude: ['password', 'isAdmin'] },
     },
 });
+
+// Définir la relation User -> Rhyme
 User.hasMany(Rhyme, { foreignKey: 'userId', sourceKey: 'id' });
-//#endregion
-//#region sync table with database
-Rhyme.sync();
-User.sync();
-//#endregion
+
+// Synchroniser les modèles avec la base de données
+sequelize.sync({ alter: true }) // Utiliser alter pour mettre à jour les tables existantes sans les recréer
+    .then(() => console.log('Database & tables created!'))
+    .catch((error) => console.error('Error creating database & tables:', error));
 
 export const db = sequelize;
 export const UserTable = User;
